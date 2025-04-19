672b0c93205d7f5bb384ccd2f4b9fd1d9caea0b8
mahesh jamdade <maheshmn121@gmail.com>
2025-04-19 18:08:12 -0400

    update grid and list ui

--- src/components/common/DogGridCard.tsx
+++ src/components/common/DogGridCard.tsx
@@ -1,13 +1,21 @@
 import { Dog } from "src/types/types";
 
 const DogGridCard = ({ dog }: { dog: Dog }) => (
-    <div className='flex flex-col items-center justify-center bg-white rounded-md shadow-md px-4 py-3 min-w-xs max-w-xs mb-4'>
+    <div className='flex flex-col bg-white rounded-md shadow-md px-4 py-3 min-w-xs max-w-xs mb-4'>
         <img src={dog.img} alt={dog.name} className='w-full h-full object-cover' />
-        <div className='flex flex-col items-center justify-center'>
-            <p className='text-lg font-bold'>{dog.name}</p>
-            <p className='text-sm text-gray-500'>{dog.breed}</p>
-            <p className='text-sm text-gray-500'>{dog.age} years</p>
+        <div className='flex items-center justify-between py-2'>
+            {/* span with year in large and years in small text */}
+            <div className='flex flex-col items-center justify-center'>
+                <p className='text-2xl text-gray-500'>{dog.age}</p>
+                <p className='text-sm text-gray-500 '>&nbsp;years</p>
+            </div>
+            <div className='flex flex-col items-center justify-center'>
+                <p className='text-lg font-bold'>{dog.name}</p>
+                <p className='text-sm text-gray-500'>({dog.breed})</p>
+            </div>
+            <p className='text-sm text-gray-500'>{dog.zip_code}</p>
         </div>
+
     </div>
 )
 
--- src/components/common/DogListCard.tsx
+++ src/components/common/DogListCard.tsx
@@ -2,13 +2,14 @@ import { Dog } from "src/types/types";
 
 const DogListCard = ({ dog }: { dog: Dog }) => (
     <div className="bg-white rounded-md shadow-md px-4 py-3 w-full max-w-md mb-4">
-        <div className="flex items-center space-x-4">
+        <div className="flex items-center space-x-4 justify-between">
             <img src={dog.img} alt={dog.name} className="w-16 h-16 rounded-full object-cover" />
-            <div>
+            <div className="flex flex-col grow">
                 <p className="text-lg font-medium text-gray-800">{dog.name}</p>
                 <p className="text-sm text-gray-600">Breed: {dog.breed}</p>
                 <p className="text-sm text-gray-600">Age: {dog.age} years</p>
             </div>
+            <p className="text-sm text-gray-600">Zip {dog.zip_code}</p>
         </div>
     </div>
 );
